39c280939c3b4be31d955c22044c5511751d3d81
Voon Shun Zhi <voonshunzhi@Voons-MacBook-Air.local>
2020-01-01 13:43:17 +0800

    Familiarizing with VueJs

---
+++ src/assets/HelloWorld.js
@@ -0,0 +1,7 @@
+
+window.onload = function() {
+    var button = document.getElementById("hello")
+    button.addEventListener('click', function() {
+        window.console.log("Nani nfenfo")
+    })
+}
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    devServer: {
+      disableHostCheck: true
+    },
+    configureWebpack: {
+      module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            use: [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.js$/,
+            use: "babel-loader",
+            exclude: /node_modules/,
+          },
+        ]
+      },
+    }
+  }
